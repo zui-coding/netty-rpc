b0674b0d858ae27ce16d01f66818de7aedabc250
Stephen.lin <linyajun@yinker.com>
2017-09-21 19:37:51 +0800

    添加demo

---
+++ rpc-proxy/src/main/java/com/zuicoding/platform/rpc/proxy/impl/JdkProviderProxy.java
@@ -0,0 +1,18 @@
+package com.zuicoding.platform.rpc.proxy.impl;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by Stephen.lin on 2017/9/21.
+ * <p>
+ * Description :<p></p>
+ */
+public class JdkProviderProxy implements InvocationHandler  {
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
